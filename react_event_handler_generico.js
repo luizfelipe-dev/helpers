2a83931fc58587bdd0919ad718d78e2d43d2f94b
Luiz Felipe Araujo <78622039+luizfelipe-dev@users.noreply.github.com>
2021-06-07 16:15:19 -0300

    Uma forma simples de utilizar um handler genérico
    Utilizando interpolação

---
+++ react_event_handler_generico.js
@@ -0,0 +1,8 @@
+handleChange({ target }) {
+  const { name } = target;
+  const value = target.type === 'checkbox' ? target.checked : target.value;
+
+  this.setState({
+    [name]: value,
+  });
+}
